ca088e54bbdfa28b5eb17c92cfb807fd06aba7ba
Felix Marinau <marinaufelixdumitru@gmail.comgit config --global --listgit config --global user.email marinaufelixdumitru@gmail.com>
2023-03-29 14:55:34 +0300

    Deleting model and repository files that were implemented in master branch

--- src/main/java/product/api/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package product.api.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import product.api.product.model.Product;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
